f5bf07a8fc4b549731c880e1eb864397af06b58b
Your Name <you@example.com>
2025-12-18 16:24:27 +0700

    f

---
+++ src/main/java/com/github/jimtrung/theater/controller/PromotionController.java
@@ -0,0 +1,41 @@
+package com.github.jimtrung.theater.controller;
+
+import com.github.jimtrung.theater.dto.PromotionRequest;
+import com.github.jimtrung.theater.service.PromotionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/promotions")
+public class PromotionController {
+
+    private final PromotionService promotionService;
+
+    public PromotionController(PromotionService promotionService) {
+        this.promotionService = promotionService;
+    }
+
+    // Lấy tất cả danh sách khuyến mãi
+    @GetMapping
+    public ResponseEntity<List<PromotionRequest>> getAllPromotions() {
+        List<PromotionRequest> promotions = promotionService.getAllPromotionRequests();
+        if (promotions.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(promotions);
+    }
+
+    // Lấy danh sách các khuyến mãi đang diễn ra (Active)
+    @GetMapping("/active")
+    public ResponseEntity<List<PromotionRequest>> getActivePromotions() {
+        List<PromotionRequest> activePromotions = promotionService.getActivePromotions();
+        if (activePromotions.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(activePromotions);
+    }
+}
